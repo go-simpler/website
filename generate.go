19dd764d5d8cdcd7753b04436d26af7865ab9bf7
Tom <73077675+tmzane@users.noreply.github.com>
2023-11-07 23:53:18 +0300

    add index.html generator

--- generate.go
+++ generate.go
@@ -1,25 +1,48 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"net/http"
 	"os"
-	"path/filepath"
-	"text/template"
+	"strings"
 )
 
-var tmpl = template.Must(template.New("").Parse(`<!doctype html>
+var indexTmpl = template.Must(template.New("").Parse(`<!doctype html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <meta
-            name="go-import"
-            content="go-simpler.org/{{.}} git https://github.com/go-simpler/{{.}}"
-        />
-        <meta
-            http-equiv="refresh"
-            content="0; url=https://pkg.go.dev/go-simpler.org/{{.}}"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="styles.css" />
+        <title>go-simpler.org</title>
+    </head>
+
+    <body>
+        <div id="content">
+            <div class="center">
+                <h2>go-simpler.org</h2>
+                <p>A collection of Go packages built with ❤️</p>
+            </div>
+            <table>
+                {{range . -}}
+                <tr>
+                    <td><a href="{{.Name}}.html">go-simpler.org/{{.Name}}</a></td>
+                    <td>{{.Desc}}</td>
+                </tr>
+                {{end}}
+            </table>
+        </div>
+    </body>
+</html>
+`))
+
+var pageTmpl = template.Must(template.New("").Parse(`<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="go-import" content="go-simpler.org/{{.}} git https://github.com/go-simpler/{{.}}" />
+        <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/go-simpler.org/{{.}}" />
     </head>
 </html>
 `))
@@ -32,27 +55,54 @@ func main() {
 }
 
 func run() error {
-	pages, err := os.Open("pages.txt")
+	data, err := os.ReadFile("pages.txt")
 	if err != nil {
 		return err
 	}
-	defer pages.Close()
 
-	sc := bufio.NewScanner(pages)
-	for sc.Scan() {
-		name := sc.Text()
-		f, err := os.Create(filepath.Join("docs", name+".html"))
-		if err != nil {
-			return err
-		}
-		if err := tmpl.Execute(f, name); err != nil {
-			return err
-		}
-		_ = f.Close()
-	}
-	if err := sc.Err(); err != nil {
+	if err := os.Chdir("docs"); err != nil {
 		return err
 	}
 
-	return nil
+	names := strings.Split(strings.TrimSpace(string(data)), "\n")
+	pages := make([]struct{ Name, Desc string }, len(names))
+
+	for i, name := range names {
+		err := func() error {
+			resp, err := http.Get("https://api.github.com/repos/go-simpler/" + name)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			var v struct {
+				Desc string `json:"description"`
+			}
+			if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
+				return err
+			}
+
+			pages[i].Name = name
+			pages[i].Desc = v.Desc
+
+			f, err := os.Create(name + ".html")
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			return pageTmpl.Execute(f, name)
+		}()
+		if err != nil {
+			return err
+		}
+	}
+
+	f, err := os.Create("index.html")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return indexTmpl.Execute(f, pages)
 }
